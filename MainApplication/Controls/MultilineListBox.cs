dd1d2f80d13b82f2a68a182b79cafdc88c235ad8
Marcin Szeniak <marc200333@gmail.com>
2017-02-12 17:05:44 +0100

    Improved appearance of the verse list

--- MainApplication/Controls/MultilineListBox.cs
+++ MainApplication/Controls/MultilineListBox.cs
@@ -5,6 +5,7 @@
     This class contains free-to-use code written by Nishant S [nishforever@vsnl.com]
 */
 
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -12,14 +13,20 @@ namespace TextToScreen.Controls
 {
     public sealed class MultilineListBox : ListBox
     {
+        private const TextFormatFlags DrawTextFlags =
+            TextFormatFlags.Left | TextFormatFlags.PreserveGraphicsClipping | 
+            TextFormatFlags.TextBoxControl | TextFormatFlags.WordBreak;
+
         public MultilineListBox()
         {
             DrawMode = DrawMode.OwnerDrawVariable;
             ScrollAlwaysVisible = true;
-            //tbox.Hide();
-            //tbox.mllb = this;
-            //Controls.Add(tbox);
-            EnabledChanged += (x, y) => BackColor = Enabled ? SystemColors.Window : SystemColors.Control;
+        }
+
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            BackColor = Enabled ? SystemColors.Window : SystemColors.Control;
+            base.OnEnabledChanged(e);
         }
 
         protected override void OnDrawItem(DrawItemEventArgs e)
@@ -29,19 +36,30 @@ namespace TextToScreen.Controls
             if (e.Index > -1)
             {
                 var s = Items[e.Index].ToString();
-
                 if ((e.State & DrawItemState.Focus) == 0)
                 {
                     e.Graphics.FillRectangle(new SolidBrush(SystemColors.Window), e.Bounds);
-                    e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.WindowText), e.Bounds);
-                    e.Graphics.DrawRectangle(new Pen(SystemColors.Highlight), e.Bounds);
+                    TextRenderer.DrawText(e.Graphics, s, Font, GetTextBounds(e.Bounds), SystemColors.WindowText, DrawTextFlags);
+                    //e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.WindowText), e.bounds);
+                    e.Graphics.DrawRectangle(new Pen(SystemColors.ControlDarkDark), GetItemBounds(e.Bounds));
                 }
                 else
                 {
-                    e.Graphics.FillRectangle(new SolidBrush(SystemColors.Highlight), e.Bounds);
-                    e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.HighlightText), e.Bounds);
+                    e.Graphics.FillRectangle(new SolidBrush(SystemColors.Highlight), GetItemBounds(e.Bounds));
+                    TextRenderer.DrawText(e.Graphics, s, Font, GetTextBounds(e.Bounds), SystemColors.HighlightText, DrawTextFlags);
+                    //e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.HighlightText), e.bounds);
                 }
             }
+        }
+
+        private static Rectangle GetItemBounds(Rectangle bounds)
+        {
+            return new Rectangle(bounds.X, bounds.Y, bounds.Width - 3, bounds.Height);
+        }
+
+        private static Rectangle GetTextBounds(Rectangle bounds)
+        {
+            return new Rectangle(bounds.X + 3, bounds.Y + 5, bounds.Width - 6, bounds.Height - 2);
         }
 
         protected override void OnMeasureItem(MeasureItemEventArgs e)
@@ -51,9 +69,10 @@ namespace TextToScreen.Controls
             if (e.Index > -1)
             {
                 var itemText = Items[e.Index].ToString();
-                var sf = e.Graphics.MeasureString(itemText, Font, Width);
+                var sf = TextRenderer.MeasureText(e.Graphics, itemText, Font, new Size(Width - 20, 0), DrawTextFlags);
+                //var sf = e.Graphics.MeasureString(itemText, Font, Width);
                 var htex = e.Index == 0 ? 15 : 10;
-                e.ItemHeight = (int) sf.Height + htex;
+                e.ItemHeight = sf.Height + htex;
                 e.ItemWidth = Width;
             }
         }
