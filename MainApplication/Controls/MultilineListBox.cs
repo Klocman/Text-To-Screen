532b47372cf3fd7d92dcdaef343886229d6bbd2b
Marcin Szeniak <marc200333@gmail.com>
2018-04-02 18:13:01 +0200

    Added verse numbers to the verse list. Closes #1

--- MainApplication/Controls/MultilineListBox.cs
+++ MainApplication/Controls/MultilineListBox.cs
@@ -14,15 +14,19 @@ namespace TextToScreen.Controls
     public sealed class MultilineListBox : ListBox
     {
         private const TextFormatFlags DrawTextFlags =
-            TextFormatFlags.Left | TextFormatFlags.PreserveGraphicsClipping | 
+            TextFormatFlags.Left | TextFormatFlags.PreserveGraphicsClipping |
             TextFormatFlags.TextBoxControl | TextFormatFlags.WordBreak;
+
+        private const TextFormatFlags DrawNumberTextFlags = DrawTextFlags | TextFormatFlags.Right;
+
+        private const int NumberBoxWidth = 24;
 
         public MultilineListBox()
         {
             DrawMode = DrawMode.OwnerDrawVariable;
             ScrollAlwaysVisible = true;
         }
-
+        
         protected override void OnEnabledChanged(EventArgs e)
         {
             BackColor = Enabled ? SystemColors.Window : SystemColors.Control;
@@ -31,35 +35,54 @@ namespace TextToScreen.Controls
 
         protected override void OnDrawItem(DrawItemEventArgs e)
         {
+            // Don't draw in forms preview
             if (Site != null)
                 return;
+
             if (e.Index > -1)
             {
                 var s = Items[e.Index].ToString();
                 if ((e.State & DrawItemState.Focus) == 0)
                 {
+                    // Draw normal verse
                     e.Graphics.FillRectangle(new SolidBrush(SystemColors.Window), e.Bounds);
                     TextRenderer.DrawText(e.Graphics, s, Font, GetTextBounds(e.Bounds), SystemColors.WindowText, DrawTextFlags);
-                    //e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.WindowText), e.bounds);
                     e.Graphics.DrawRectangle(new Pen(SystemColors.ControlDarkDark), GetItemBounds(e.Bounds));
+
                 }
                 else
                 {
+                    // Draw selected verse
                     e.Graphics.FillRectangle(new SolidBrush(SystemColors.Highlight), GetItemBounds(e.Bounds));
                     TextRenderer.DrawText(e.Graphics, s, Font, GetTextBounds(e.Bounds), SystemColors.HighlightText, DrawTextFlags);
-                    //e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.HighlightText), e.bounds);
                 }
+
+                // Draw verse number
+                var verseNumber = (e.Index + 1).ToString();
+                e.Graphics.FillRectangle(new SolidBrush(Color.DarkGray), GetNumberBoxBounds(e.Bounds));
+                TextRenderer.DrawText(e.Graphics, verseNumber, Font, GetNumberTextBounds(e.Bounds), Color.LightGray, DrawNumberTextFlags);
+                e.Graphics.DrawRectangle(new Pen(SystemColors.ControlDarkDark), GetNumberBoxBounds(e.Bounds));
             }
+        }
+
+        private static Rectangle GetNumberBoxBounds(Rectangle bounds)
+        {
+            return new Rectangle(bounds.X, bounds.Y, NumberBoxWidth, bounds.Height);
+        }
+
+        private static Rectangle GetNumberTextBounds(Rectangle bounds)
+        {
+            return new Rectangle(bounds.X , bounds.Y + 5, NumberBoxWidth, bounds.Height - 2);
         }
 
         private static Rectangle GetItemBounds(Rectangle bounds)
         {
-            return new Rectangle(bounds.X, bounds.Y, bounds.Width - 3, bounds.Height);
+            return new Rectangle(bounds.X + NumberBoxWidth, bounds.Y, bounds.Width - 3 - NumberBoxWidth, bounds.Height);
         }
 
         private static Rectangle GetTextBounds(Rectangle bounds)
         {
-            return new Rectangle(bounds.X + 3, bounds.Y + 5, bounds.Width - 6, bounds.Height - 2);
+            return new Rectangle(bounds.X + 3 + NumberBoxWidth, bounds.Y + 5, bounds.Width - 6 - NumberBoxWidth, bounds.Height - 2);
         }
 
         protected override void OnMeasureItem(MeasureItemEventArgs e)
